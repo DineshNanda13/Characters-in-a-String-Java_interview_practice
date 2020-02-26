8e73e765039fc7ca6869c876567e51d0dfd84cb7
Dinesh Nanda <dannynanda76@gmail.com>
2020-02-25 22:41:17 -0500

    num of characters in a string

---
+++ Num of chars in a string~/CharsInString/src/main/java/charsinstring/CharsInString.java
@@ -0,0 +1,24 @@
+package charsinstring;
+
+/**
+ *
+ * @author Dinesh Nanda
+ */
+
+public class CharsInString {
+
+    public static void main(String[] args) {
+        
+        String string = "The best of both worlds";
+        int count = 0;
+        
+        for(int i = 0; i < string.length(); i++){
+            
+            if(string.charAt(i) != ' '){
+                count++;
+            }
+        }
+        System.out.println("The number of characters in a given string is: "+ count);
+    }
+    
+}
